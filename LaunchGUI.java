979a99e6bbf4d94cf6c995a048fba8f6ccee7bdc
Jenny Zhang <jennyzhangqianni@gmail.com>
2020-02-27 15:43:30 -0700

    Minor modifications

--- LaunchGUI.java
+++ LaunchGUI.java
@@ -2,9 +2,6 @@ import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-
 public class LaunchGUI {
 
 	private LoginFrame loginFrame;
