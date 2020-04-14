956e53d9bf2a6210c841b3410a05480c6eb90dc7
Jenny Zhang <jennyzhangqianni@gmail.com>
2020-04-13 20:17:45 -0500

    Missing 07:00 as time

--- src/NurseView.java
+++ src/NurseView.java
@@ -91,7 +91,7 @@ public class NurseView extends JFrame{
 	private JComboBox<String>[] availTimes = new JComboBox[14];	//stores al drop-down menues for availability change; index 0= sunday start // index 13= sat end
 	
 	String[] times = {
-			"00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "08:00", "09:00", "10:00", "11:00", "12:00"
+			"00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00"
 			, "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"
 	};
 	
